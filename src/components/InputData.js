f4d8fb51e63c6a5bf283af35da6777217b9da2b7
marceloremeseiro <marcelo.remeseiro@gmail.com>
2024-09-30 01:51:58 +0200

    InputData

--- src/components/InputData.js
+++ src/components/InputData.js
@@ -1,34 +1,39 @@
 import CopyButton from "./CopyButton";
 
 export default function InputData({ input }) {
+  const url = "https://streamingpro.es";
+  const port = "6000";
+  const latency = "120ms";
+  const tipo = "caller";
+  const streamId = input.reference;
   return (
     <div className="bg-gray-700 p-4 rounded-lg mb-4">
       <div className="grid grid-cols-2 gap-4">
         <div>
           <p className="text-gray-400 font-semibold">URL</p>
           <p className="text-sm text-gray-300 break-all">
-            srt://streamingpro.es
+            srt://{url}:{port}
           </p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">PORT</p>
-          <p className="text-sm text-gray-300">6000</p>
+          <p className="text-sm text-gray-300">{port}</p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">LATENCY</p>
-          <p className="text-sm text-gray-300">120ms</p>
+          <p className="text-sm text-gray-300">{latency}</p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">TIPO</p>
-          <p className="text-sm text-gray-300">CALLER</p>
+          <p className="text-sm text-gray-300">{tipo}</p>
         </div>
         <div className="col-span-2">
           <p className="text-gray-400 font-semibold">STREAMID</p>
           <div className="flex items-center justify-between ">
             <p className="text-sm text-gray-300 break-all">
-              {`${input.streamId}.stream,mode:publish`}
+              {`${streamId}.stream,mode:publish`}
             </p>
-            <CopyButton text={`${input.streamId}.stream,mode:publish`} />
+            <CopyButton text={`${streamId}.stream,mode:publish`} />
           </div>
         </div>
       </div>
