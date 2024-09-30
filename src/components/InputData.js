1da0ced0e155b63e00efc36f2e906ce3d7e15ba7
marceloremeseiro <marcelo.remeseiro@gmail.com>
2024-09-30 02:13:43 +0200

    inputdata

--- src/components/InputData.js
+++ src/components/InputData.js
@@ -1,29 +1,27 @@
 import CopyButton from "./CopyButton";
 
 export default function InputData({ input }) {
-  const url = "srt://streamingpro.es";
-  const port = "6000";
-  const latency = "120ms";
+  console.log(input);
   return (
     <div className="bg-gray-700 p-4 rounded-lg mb-4">
       <div className="grid grid-cols-2 gap-4">
         <div>
           <p className="text-gray-400 font-semibold">URL</p>
           <p className="text-sm text-gray-300 break-all">
-            {url}
+            {input.url || "srt://streamingpro.es"}
           </p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">PORT</p>
-          <p className="text-sm text-gray-300">{port}</p>
+          <p className="text-sm text-gray-300">{input.port || "6000"}</p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">LATENCY</p>
-          <p className="text-sm text-gray-300">{latency}</p>
+          <p className="text-sm text-gray-300">{input.latency || "120ms"}</p>
         </div>
         <div>
           <p className="text-gray-400 font-semibold">TIPO</p>
-          <p className="text-sm text-gray-300">CALLER</p>
+          <p className="text-sm text-gray-300">{input.tipo || "CALLER"}</p>
         </div>
         <div className="col-span-2">
           <p className="text-gray-400 font-semibold">STREAMID</p>
