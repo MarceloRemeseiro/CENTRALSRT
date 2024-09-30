a41b3118cd9578cfbb23fb53812b30aa66eb3d92
marceloremeseiro <marcelo.remeseiro@gmail.com>
2024-09-30 02:04:36 +0200

    InputData modif

--- src/components/InputData.js
+++ src/components/InputData.js
@@ -1,18 +1,16 @@
 import CopyButton from "./CopyButton";
 
 export default function InputData({ input }) {
-  const url = "https://streamingpro.es";
+  const url = "srt://streamingpro.es";
   const port = "6000";
   const latency = "120ms";
-  const tipo = "caller";
-  const streamId = input.reference;
   return (
     <div className="bg-gray-700 p-4 rounded-lg mb-4">
       <div className="grid grid-cols-2 gap-4">
         <div>
           <p className="text-gray-400 font-semibold">URL</p>
           <p className="text-sm text-gray-300 break-all">
-            srt://{url}:{port}
+            {url}
           </p>
         </div>
         <div>
@@ -25,15 +23,15 @@ export default function InputData({ input }) {
         </div>
         <div>
           <p className="text-gray-400 font-semibold">TIPO</p>
-          <p className="text-sm text-gray-300">{tipo}</p>
+          <p className="text-sm text-gray-300">CALLER</p>
         </div>
         <div className="col-span-2">
           <p className="text-gray-400 font-semibold">STREAMID</p>
           <div className="flex items-center justify-between ">
             <p className="text-sm text-gray-300 break-all">
-              {`${streamId}.stream,mode:publish`}
+              {`${input.streamId}.stream,mode:publish`}
             </p>
-            <CopyButton text={`${streamId}.stream,mode:publish`} />
+            <CopyButton text={`${input.streamId}.stream,mode:publish`} />
           </div>
         </div>
       </div>
